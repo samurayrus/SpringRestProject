3b04fc6e0524b38206afb18b53fc61d5c6376f0d
samurayrus <46180435+samurayrus@users.noreply.github.com>
2020-08-20 10:27:48 +0300

    Comment class

---
+++ springrestproject/src/main/java/ru/innotechnum/controllers/entity/Comment.java
@@ -0,0 +1,107 @@
+package ru.innotechnum.controllers.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="COMMENTS")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "text")
+    private String text;
+    @Column(name = "raiting")
+    private int raiting;
+    @Column(name = "authorId")
+    private int authorId;
+    @Column(name = "authName")
+    private String authName;
+    @Column(name = "publicationId")
+    private int publicationId;
+    @Column(name = "DateCreate")
+    private LocalDate DateCreate;
+    @Column(name = "parentId")
+    private int parentId;
+
+    public Comment() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getRaiting() {
+        return raiting;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public String getAuthName() {
+        return authName;
+    }
+
+    public int getPublicationId() {
+        return publicationId;
+    }
+
+    public LocalDate getDateCreate() {
+        return DateCreate;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setRaiting(int raiting) {
+        this.raiting = raiting;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setAuthName(String authName) {
+        this.authName = authName;
+    }
+
+    public void setPublicationId(int publicationId) {
+        this.publicationId = publicationId;
+    }
+
+    public void setDateCreate(LocalDate dateCreate) {
+        DateCreate = dateCreate;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", raiting=" + raiting +
+                ", authorId=" + authorId +
+                ", authName='" + authName + '\'' +
+                ", publicationId=" + publicationId +
+                ", DateCreate=" + DateCreate +
+                ", parentId=" + parentId +
+                '}';
+    }
+}
